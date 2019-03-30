dd1f640cf863c4ec03d1bcc4916a3aab9ffcae1c
Heberto Herrada <haherrada@gmail.com>
2019-03-30 15:21:54 -0500

    added delete tasks, remove all tasks, and filter tasks functionality

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   //Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter task event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -41,4 +47,40 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  //While loop = better performance
+  if(confirm('Are you sure you want to clear your task list?')) {
+    while(taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+  //Can also do this, simpler but worse performance
+  //taskList.innerHTML = '';
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    function(task) {
+      const item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+      }
+    });
 }
